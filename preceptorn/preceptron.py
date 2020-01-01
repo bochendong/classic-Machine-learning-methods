8f8698b33845c8989672fe9dc5714b25d6879587
bochendong <35963069+bochendong@users.noreply.github.com>
2020-01-01 09:47:01 +0800

    Update preceptron.py

--- preceptorn/preceptron.py
+++ preceptorn/preceptron.py
@@ -40,36 +40,3 @@ y = np.loadtxt(open("spambase_y.csv", "rb"), delimiter=",")
 
 prec = Preception(x, y)
 prec.show()
-
-'''
-def preception (x, y):
-    n = len(y)
-    d = len(x)
-
-    # init w as [0 0 ... 0 0]
-    w = np.array([0]* d).reshape(d,1)
-    # init b = 0
-    b = 0
-
-    max_pass = 500
-    mistake = [0] * max_pass
-
-    for t in range(0, max_pass):
-        for i in range (0, n):
-            if (y[i] * (np.vdot(x[:,i], w) + b) <= 0) :
-                w = w + (y[i] * x[:,i]).reshape(d,1)
-                b = b + y[i]
-                mistake[t] = mistake[t] + 1
-                
-    x_axis = []
-    for i in range(0,500):
-        x_axis.append(i)
-        
-    plt.scatter(x_axis,mistake)
-    plt.xlabel('Passes')
-    plt.ylabel('Mistakes')
-    plt.title('Perceptron')
-    plt.show()                
-
-preception (x, y)
-'''
